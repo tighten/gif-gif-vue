b7f0ff014a3bc3149376c10f918bfc20c2b28c03
Samantha Geitz <samanthamichele7@gmail.com>
2018-07-20 15:15:01 -0500

    Add testing presets and Tailwind

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex items-center">
+        <div class="md:w-1/2 md:mx-auto"></div>
+    </div>
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
-let mix = require('laravel-mix');
+const { mix } = require('laravel-mix');
+require('laravel-mix-purgecss');
+require('laravel-mix-tailwind');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +14,12 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .tailwind()
+    .purgeCss();
+
+mix.webpackConfig({
+    resolve: {
+        symlinks: false
+    }
+});
